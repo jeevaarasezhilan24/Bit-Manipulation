31014166de48580053bbac9811ceb323d6ef947c
Jeeva Arasezhilan <jeevaarasezhilan@gmail.com>
2026-09-03 09:18:18 +0530

    Add files via upload

---
+++ k & k+1th set bit.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int count = 0;
+        for (int x : arr) {
+            boolean kth=(x&(1<<k))!=0;
+            boolean next=(x&(1<<(k+1)))!=0;
+            if (kth && next) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
